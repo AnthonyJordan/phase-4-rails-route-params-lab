4dc2c6bd82278c45a26523ca1ddec5b2a1e0de6f
Anthony <Kain235@gmail.com>
2022-06-27 12:27:03 -0400

    commit tree 2f370bcd51b6597af89f173112c4b2614ed6ebea

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
+    if(params[:name])
+      students = Student.where("first_name =? or last_name =?",params[:name].capitalize , params[:name].capitalize).all
+      return render json: students
+    end
     students = Student.all
     render json: students
   end
-
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
